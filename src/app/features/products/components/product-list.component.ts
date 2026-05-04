f6e4293ac8a64841ce6e69406632ef12ee018dae
siddharth gelda <geldasiddharth@gmail.com>
2026-05-05 02:54:24 +0530

    Ecomm UI

--- src/app/features/products/components/product-list.component.ts
+++ src/app/features/products/components/product-list.component.ts
@@ -148,7 +148,7 @@ export class ProductListComponent implements OnInit, OnDestroy {
 //   minPrice:          number | undefined;
 //   maxPrice:          number | undefined;
   minPrice: number = 0;
-    maxPrice: number = 0;
+    maxPrice: number = 100000;
   sortBy             = '';
 
   private destroy$ = new Subject<void>();
@@ -213,7 +213,7 @@ export class ProductListComponent implements OnInit, OnDestroy {
   clearFilters(): void {
     this.selectedCategory = null;
     this.minPrice         = 0;
-    this.maxPrice         = 0;
+    this.maxPrice         = 10000;
     this.sortBy           = '';
     this.currentPage      = 0;
     this.loadProducts();
